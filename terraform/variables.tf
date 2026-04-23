84863ac0a8ad2c053397a37e769b4541e72cf1c2
Noam-Journo <noamjorno6@gmail.com>
2026-04-23 17:08:44 +0300

    edit envs/dev/values.yaml

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,8 +1,12 @@
 variable "environment" {
-  description = "dev / staging / prod"
   type        = string
-}
+  description = "dev/staging/prod"
 
+  validation {
+    condition     = contains(["dev", "staging", "prod"], var.environment)
+    error_message = "Environment must be dev, staging, or prod."
+  }
+}
 variable "aws_region" {
   default = "eu-central-1"
 }
